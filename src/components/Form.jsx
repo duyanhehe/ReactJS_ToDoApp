9e23fccd66285ab40c26298a0679935f82a2ccd0
duyanhehe <duyanhex@gmail.com>
2024-09-13 13:33:09 +0700

    add double click to edit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -15,7 +15,7 @@ export default function Form({ todos, setTodos }){
 
     return(
         <div>
-            <form onSubmit={handleSubmit} className="inputContainer">
+            <form onSubmit={handleSubmit}>
                 <div>
                     <input className="inputField" onChange={(e) => setTodo({name:e.target.value, done:false})} value={todo.name} type="text" placeholder="What needs to be done?" />
                 </div>
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,10 @@
 import "../css/app.sass"
+import { useState } from "react";
 
 export default function TodoItem({item, todos, setTodos}){
+    const [isEditing, setIsEditing] = useState(false);
+    const [newTodo, setNewTodo] = useState(item.name);
+
     function handleDelete(item){
         console.log(item);
         setTodos(todos.filter((todo) => todo !== item));
@@ -10,11 +14,25 @@ export default function TodoItem({item, todos, setTodos}){
         setTodos(todos.map((todo) => todo.name === name? {...todo, done:!todo.done} : todo));
     }
 
-    // const finished = item.done ? styles.completed : "";
+    function handleDoubleClick(){
+        setIsEditing(true);
+    }
+
+    function handleBlur() {
+        if (newTodo.trim() === '') return;
+        setTodos(todos.map((todo) => todo.name === item.name ? { ...todo, name: newTodo} : todo));
+        setIsEditing(false);
+    }
+
+    function handleKeyDown(e) {
+        if (e.key === 'Enter') {
+            handleBlur();
+        }
+    }
 
     return(
-        <div>
-            <div>
+        <div onDoubleClick={handleDoubleClick} className="todosContainer">
+            <div className="todoItem">
                 <label>
                     <input
                     type="checkbox"
@@ -22,14 +40,24 @@ export default function TodoItem({item, todos, setTodos}){
                     onChange={() => handleClick(item.name)}
                     />
                 </label>
-                <span className={item.done ? 'completed' : ''}>
-                    {item.name}
-                </span>
+                {isEditing ? (
+                    <input
+                        type="text"
+                        value={newTodo}
+                        onChange={(e) => setNewTodo(e.target.value)}
+                        onBlur={handleBlur}
+                        onKeyDown={handleKeyDown}
+                        autoFocus
+                    />
+                ) : (
+                    <span className={item.done ? 'completed' : ''}>
+                        {item.name}
+                    </span>
+                )}
                 <span>
                     <button className="deleteTodo" onClick={() => handleDelete(item)}>x</button>
                 </span>
             </div>
-            <hr />
         </div>
     )
 }
